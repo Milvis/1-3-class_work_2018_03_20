74c3d94ac18495f1e569251690eb8ed171419c0e
Milvis <info@milvis.lt>
2018-03-29 19:28:19 +0300

    atnaujinti fx siektiek su enter

--- FXZaidimas/src/sample/Controller.java
+++ FXZaidimas/src/sample/Controller.java
@@ -5,7 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 
+import java.awt.event.KeyEvent;
 import java.util.InputMismatchException;
 import java.util.Random;
 
@@ -15,23 +17,32 @@ public class Controller {
     @FXML
     private TextField ivedimoLaukas;
 
-    public void spejimas(ActionEvent event) { //actionevent -javafx
 
+    public void spejimas(ActionEvent event) { //actionevent -javafx
+        allLogic();
+    }
+//paspaudus Enter suveiks paleidimo mygtukas
+    public void enterClicked(javafx.scene.input.KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER && !ivedimoLaukas.getText().equals("")) {
+            allLogic();
+        }
+    }
+
+    private void allLogic() {
         Random rand = new Random();
         int randomNumber = rand.nextInt(10) + 1; //kad isvestu 10 skaiciu o ne iki 9 ir be 0.
         try {
             int sk = Integer.valueOf(ivedimoLaukas.getText()); //string konvert i integer nes i textfield tik string isiveda tada paverciam i int
             ivedimoLaukas.setText("");
-            if(sk<=10){
-            if (randomNumber == sk) {
-                //   System.out.println("Jus atspejote skaicius buvo "+sk);
-                isvedimas.setText("Jūs atspėjote " + "\nSugeneruotas skaičius buvo " + randomNumber + "\nJūsų spėtas skaičius buvo " + sk);
+            if (sk <= 10) {
+                if (randomNumber == sk) {
+                    //   System.out.println("Jus atspejote skaicius buvo "+sk);
+                    isvedimas.setText("Jūs atspėjote " + "\nSugeneruotas skaičius buvo " + randomNumber + "\nJūsų spėtas skaičius buvo " + sk);
+                } else {
+                    //   System.out.println("Spekite dar karta");
+                    isvedimas.setText("Spėkite dar kartą, sugeneruotas skaičius buvo " + randomNumber + "\nJūsų spėtas skaičius buvo " + sk);
+                }
             } else {
-                //   System.out.println("Spekite dar karta");
-                isvedimas.setText("Spėkite dar kartą, sugeneruotas skaičius buvo " + randomNumber + "\nJūsų spėtas skaičius buvo " + sk);
-            }
-            }
-            else{
                 isvedimas.setText("Ivestas netinkamas skaičius.\nŽaidimas spėja skaičius nuo 1 iki 10");
             }
 
---
+++ src/tKlase.java
@@ -0,0 +1,13 @@
+public class tKlase {
+    private String pavadinimas;
+    private String marke;
+    private String modelis;
+    private int pagaminimoMetai;
+
+    public tKlase(String pavadinimas, String marke, String modelis, int pagaminimoMetai) {
+        this.pavadinimas = pavadinimas;
+        this.marke = marke;
+        this.modelis = modelis;
+        this.pagaminimoMetai = pagaminimoMetai;
+    }
+}
