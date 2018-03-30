1223931afb601f586c2fc32f33bc5d4efa4bcd4c
Milvis <info@milvis.lt>
2018-03-30 21:32:01 +0300

    java web pirma paskaita

---
+++ webjava/src/MyFirstServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(value="/myFirstServlet")
+public class MyFirstServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //System.out.println("Atejome I SERVLETA");
+        String name="Andrius";
+        String surname="Baltrunas";
+
+        req.setAttribute("name",name);
+        req.setAttribute("surname",surname);
+        req.getRequestDispatcher("myFirstJsp.jsp").forward(req,resp);
+
+    }
+}
---
+++ webjava/web/myFirstJsp.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: v15nitro
+  Date: 2018-03-30
+  Time: 20:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<div>Vatotojo_vardas:${name}</div>
+
+<div>Vartotojo_pavarde:${surname}</div>
+
+</body>
+</html>
